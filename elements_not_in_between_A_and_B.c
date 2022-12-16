7d9a10023800bbcd571d3dede1e72d2b50b571e3
mrkiranilla <120078089+mrkiranilla@users.noreply.github.com>
2022-12-16 10:32:12 +0530

    #include<stdio.h> int main() {     int n,i,x,y,c=0;     scanf("%d",&n);     int a[n];     for(i=0;i<n;i++)     {         scanf("%d",&a[i]);     }     scanf("%d%d",&x,&y);     for(i=0;i<n;i++)     {         if(a[i]<x || a[i]>y)         {             printf("%d ",a[i]);             c++;         }     }     if(c==0)     {         printf("-1");     }
    }

---
+++ elements_not_in_between_A_and_B.c
@@ -0,0 +1,25 @@
+#include<stdio.h>
+int main()
+{
+    int n,i,x,y,c=0;
+    scanf("%d",&n);
+    int a[n];
+    for(i=0;i<n;i++)
+    {
+        scanf("%d",&a[i]);
+    }
+    scanf("%d%d",&x,&y);
+    for(i=0;i<n;i++)
+    {
+        if(a[i]<x || a[i]>y)
+        {
+            printf("%d ",a[i]);
+            c++;
+        }
+    }
+    if(c==0)
+    {
+        printf("-1");
+    }
+    
+}
