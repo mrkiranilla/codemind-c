d76e69fc3f2d5eab1668a05e0716f907693574cc
mrkiranilla <120078089+mrkiranilla@users.noreply.github.com>
2023-01-09 09:41:59 +0530

    #include<stdio.h> int main() {     char s[100];     int i,c=1;     scanf("%[^ ]s",s);     for(i=0;s[i]!=NULL;i++)     {         if(s[i]==' ')         {             c++;         }     }     printf("%d",c);
    }

---
+++ Count_no._of_words.c
@@ -0,0 +1,17 @@
+#include<stdio.h>
+int main()
+{
+    char s[100];
+    int i,c=1;
+    scanf("%[^
+]s",s);
+    for(i=0;s[i]!=NULL;i++)
+    {
+        if(s[i]==' ')
+        {
+            c++;
+        }
+    }
+    printf("%d",c);
+    
+}
