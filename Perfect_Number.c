b4acdea52ee29d6e42309fcdc2b6468efaa1fcf2
mrkiranilla <120078089+mrkiranilla@users.noreply.github.com>
2023-05-05 11:23:26 +0530

    #include<stdio.h> int main() {     int n,i,t,sum=0;     scanf("%d",&n);     t=n;     for(i=1;i<=(n/2)+1;i++)     {         if(n%i==0)         {             sum=sum+i;         }     }     if(sum==t)     {         printf("True");     }     else     {         printf("False");     } }

--- Perfect_Number.c
+++ Perfect_Number.c
@@ -1,18 +1,19 @@
 #include<stdio.h>
 int main()
 {
-    int n,i,sum=0;
+    int n,i,t,sum=0;
     scanf("%d",&n);
-    for(i=1;i<n;i++)
+    t=n;
+    for(i=1;i<=(n/2)+1;i++)
     {
         if(n%i==0)
         {
-           sum=sum+i; 
+            sum=sum+i; 
         }
     }
-    if(sum==n)
+    if(sum==t)
     {
-    printf("True");
+        printf("True");
     }
     else
     {
