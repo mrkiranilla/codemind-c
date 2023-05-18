9485c3c2d81dba303821fe98a0f6d8e8970e65ab
mrkiranilla <120078089+mrkiranilla@users.noreply.github.com>
2023-05-18 15:29:29 +0530

    #include<stdio.h> int main() {     int n;     scanf("%d",&n);     start:     int r,sum=0;     while(n!=0)     {         r=n%10;         sum=sum+r;         n=n/10;     }     n=sum;     if(n<10)     {         printf("%d",sum);     }     else     {         goto start;     } }

--- Add_Digits.c
+++ Add_Digits.c
@@ -1,18 +1,18 @@
 #include<stdio.h>
 int main()
 {
-    int n,r,sum=0;
+    int n;
     scanf("%d",&n);
     start:
-    sum=0;
-    while(n>0)
+    int r,sum=0;
+    while(n!=0)
     {
         r=n%10;
         sum=sum+r;
         n=n/10;
     }
     n=sum;
-    if(sum<10)
+    if(n<10)
     {
         printf("%d",sum);
     }
